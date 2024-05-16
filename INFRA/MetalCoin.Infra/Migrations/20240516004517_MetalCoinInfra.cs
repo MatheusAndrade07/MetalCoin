829bb3ed478b3f741f55664780caec5ef814c562
Matheus <matheusandradepompermayer07@gmail.com>
2024-05-15 21:51:12 -0300

    Adicionado o migration.

---
+++ INFRA/MetalCoin.Infra/Migrations/20240516004517_MetalCoinInfra.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MetalCoin.Infra.Migrations
+{
+    public partial class MetalCoinInfra : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cupons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Codigo = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Descricao = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    ValorDesconto = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TipoDesconto = table.Column<int>(type: "int", nullable: false),
+                    DataValidade = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    QuantidadeLiberada = table.Column<int>(type: "int", nullable: false),
+                    QuantidadeUsada = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cupons", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cupons");
+        }
+    }
+}
